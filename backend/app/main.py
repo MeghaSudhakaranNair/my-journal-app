c5c14c28ef8dca147cdf5c398a769316b91a4609
MeghaSudhakaranNair <megha99.sudhakaran@gmail.com>
2026-08-23 21:45:46 -0400

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://my-journal-app-sage.vercel.app/"
+        "https://my-journal-app-sage.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
